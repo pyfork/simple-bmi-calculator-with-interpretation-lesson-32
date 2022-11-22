ac7273fb65effa1069c032e7c02062ddfc9252bd
Ahmad <118096225+pyfork@users.noreply.github.com>
2022-11-22 17:29:31 +0000

    refactor: Improve use of elif

--- main.py
+++ main.py
@@ -5,22 +5,16 @@ weight = float(input("Enter your weight in kg: "))
 # Calculate BMI, round it up to next integer
 bmi = round(weight / height ** 2)
 
-# Stringify BMI
-# bmi_str = str(bmi)
-
 # Output BMI reading and interpretation
 # F-string method
 if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.\n")
-elif bmi > 18.5:
-  if bmi < 25:
-    print(f"Your BMI is {bmi}, you have a normal weight.\n")
-  elif bmi > 25:
-    if bmi < 30:
-      print(f"Your BMI is {bmi}, you are slightly overweight.\n")
-    elif bmi > 30:
-      if bmi < 35:
-        print(f"Your BMI is {bmi}, you are obese.\n")
-      elif bmi >= 35:
-        print(f"Your BMI is {bmi}, you are clinically obese.\n")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.\n")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.\n")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.\n")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.\n")
         
