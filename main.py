7333e792d431a0d0e64828e0b45ac65b85c0410c
Ahmad <118096225+pyfork@users.noreply.github.com>
2022-11-22 17:02:27 +0000

    style: Fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Get data from user
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
 
 # Calculate BMI, round it up to next integer
 bmi = round(weight / height ** 2)
@@ -10,16 +10,16 @@ bmi_str = str(bmi)
 
 # Output BMI reading and interpretation
 if bmi < 18.5:
-  print("Your BMI is " + bmi_str + ", You are underweight.\n")
+  print("Your BMI is " + bmi_str + ", you are underweight.\n")
 elif bmi > 18.5:
   if bmi < 25:
-    print("Your BMI is " + bmi_str + ", You have a normal weight.\n")
+    print("Your BMI is " + bmi_str + ", you have a normal weight.\n")
   elif bmi > 25:
     if bmi < 30:
-      print("Your BMI is " + bmi_str + ", You are slightly overweight.\n")
+      print("Your BMI is " + bmi_str + ", you are slightly overweight.\n")
     elif bmi > 30:
-      if bmi <35:
-        print("Your BMI is " + bmi_str + ", You are obese.")
+      if bmi < 35:
+        print("Your BMI is " + bmi_str + ", you are obese.\n")
       elif bmi >= 35:
-        print("Your BMI is " + bmi_str + ", You are clinically obese.")
+        print("Your BMI is " + bmi_str + ", you are clinically obese.\n")
         
